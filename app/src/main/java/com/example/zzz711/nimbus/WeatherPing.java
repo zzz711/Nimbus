03bdc28de149417a9266dc19109654a1c759f6f7
Margo Morton <mortonm@wittenberg.edu>
2014-11-16 00:01:53 -0500

    Added functions in Weather Ping, now it compiles. This class is responsible for creating notifications based on current location data.

--- app/src/main/java/com/example/zzz711/nimbus/WeatherPing.java
+++ app/src/main/java/com/example/zzz711/nimbus/WeatherPing.java
@@ -3,9 +3,12 @@ package com.example.zzz711.nimbus;
 import android.app.IntentService;
 import android.app.Service;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.IBinder;
 import android.util.Log;
@@ -14,51 +17,65 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-
+import android.os.Bundle;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
- * Created by Margo on 11/15/2014.
+ * @author Margo
+ * @version 11/15/2014
+ * Weather Ping is a service class that runs in the background of Main Activity. It's primary job
+ * is to send notifications based on current location data.
  */
-public class WeatherPing extends Service {
+public class WeatherPing extends Service implements LocationListener{
     Intent serviceIntent;
     String weatherUrl;
+    protected LocationManager locationManager;
+    private final Context currContext;
+    private String apiKey = ""; //TODO: add, but don't commit key
+    private static final long MIN_TIME_BW_UPDATES = 12600000; //  3.5 hours
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0; //no min change
+    protected Location location;
+    double latitude;
+    double longitude;
 
-    //TODO add public boolean stopService(Intent name)
-
-    private String apiKey = "";
-    /*public WeatherPing(){
-        super("WeatherPing");
+    // constructor for WeatherPing service
+    public WeatherPing(){
+        // grab the current context
+        currContext = getApplicationContext();
     }
 
-    @Override
-    protected abstract void onHandleIntent(Intent intent){}*/
+
+
+    // files necessary for LocationListener; they do nothing.
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+    public void onProviderEnabled(String provider) {}
+    public void onProviderDisabled(String provider) {}
+    public void onLocationChanged(Location location) {}
+
+
 
     @Override
-  /*  public ComponentName startService(Intent intent){
-        //need return statement
+    public ComponentName startService(Intent intent){
         //onHandleIntent(intent);
         serviceIntent = intent;
         onCreate();
+        ComponentName cn = new ComponentName(currContext, "WeatherPing");
+        return cn;
+    }
 
-    }*/
-
-    //@Override
+    @Override
     public void onCreate(){
         onStartCommand(serviceIntent, 0, 0); //start id may not be 0
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        //TODO do something useful
         if(isConnected()){
-
+            getLocation();
+            //TODO: something here
         }
-
-
         return Service.START_NOT_STICKY;
     }
 
@@ -95,6 +112,45 @@ public class WeatherPing extends Service {
         }
     }
 
+    public Location getLocation() {
+        locationManager = (LocationManager) currContext.getSystemService(LOCATION_SERVICE);
+        boolean GPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (!GPSEnabled) {
+            // TODO: what if they don't have GPS or internet?
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                    this);
+            Log.d("Network", "Network");
+            if (locationManager != null) {
+                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                setLatLong();
+            }
+            if (location == null) {
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER,
+                        MIN_TIME_BW_UPDATES,
+                        MIN_DISTANCE_CHANGE_FOR_UPDATES,
+                        this);
+                Log.d("GPS Enabled", "GPS Enabled");
+                if (locationManager != null) {
+                    location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    setLatLong();
+                }
+            }
+        }
+        return location;
+    }
+
+    public void setLatLong(){
+        if(location != null){
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+        }
+    }
+
     public String getJSONData(String url){
         InputStream inputStream = null;
         String result = "";
@@ -120,6 +176,8 @@ public class WeatherPing extends Service {
 
         return  result;
     }
+
+    //TODO add public boolean stopService(Intent name)
 
     @Override
     public IBinder onBind(Intent intent) {
